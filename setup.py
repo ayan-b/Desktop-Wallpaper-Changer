4c3ddc0bd72aab00f268f85baae32982009feb19
ayan-b <ayanbanerjee7777@gmail.com>
2018-09-21 19:08:22 +0530

    Add changes to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import os
 from setuptools import find_packages, setup
 
-requirement_list = [r.strip() 
-                        for r in open('requirements.txt', 'r').readlines() if r]
+requirement_list = [r.strip() for r in open('requirements.txt', 'r').readlines() if r]
 
 if __name__ == '__main__':
-    setup(name='dwc',
+    setup(
+          name='dwc',
           version='0.0.1',
           install_requires=requirement_list,
           description='Change your desktop wallpaper daily!',

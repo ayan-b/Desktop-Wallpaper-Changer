7f28a2ef7aa1e2b5dfba88491c73732bb139f2dc
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-04-18 11:46:54 +0530

    Add test and lint setup

---
+++ tests/test_debug.py
@@ -0,0 +1,7 @@
+from dwc import debug
+
+
+def test_human_readable_size():
+    actual = debug.human_readable_size(5)
+    expected = "5.00 bytes"
+    assert actual == expected
---
+++ tests/test_sources.py
@@ -0,0 +1,10 @@
+import dwc.sources.bing as bing
+import dwc.sources.desktoppr as desktoppr
+import dwc.sources.nasa_pod as nasa_pod
+import dwc.sources.natgeo_pod as natgeo_pod
+import dwc.sources.unsplash as unsplash
+
+
+# placeholder test
+def test_bing():
+    assert True
